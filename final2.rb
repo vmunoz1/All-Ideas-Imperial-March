ed05c25acd34a26f07ed1a9064ccdc941a942abf
vmunoz1 <45017690+vmunoz1@users.noreply.github.com>
2020-02-06 10:41:34 -0800

    Add files via upload

---
+++ final2.rb
@@ -0,0 +1,90 @@
+luke="C:/Users/valeria_munoz/Downloads/The_Imperial_March/Imperial_Samples/luke_fatherd.wav"
+noise="C:/Users/valeria_munoz/Downloads/The_Imperial_March/Imperial_Samples/noise.wav"
+darth="C:/Users/valeria_munoz/Downloads/The_Imperial_March/Imperial_Samples/sound.wav"
+
+
+use_synth  :blade
+use_bpm 50
+sample luke, amp:3
+sleep 56
+
+define :first do
+  play:G4
+  sleep 1
+  play:E4
+  sleep 0.75
+  play:Bb4
+  sleep 0.25
+  play:G4
+  sleep 2
+end
+
+notes=[:Gb4, :E4]
+i=0
+y=1
+
+define :second do
+  2.times do
+    play notes[i]
+    sleep y
+    i = i + 1
+    y = y - 0.25
+  end
+  i=0
+  y=1
+end
+
+define :small do
+  play:Bb4
+  sleep 0.25
+end
+
+live_loop :noisee do
+  34.times do
+    sample noise, rate: 0.1
+    sleep 1
+  end
+  stop
+end
+
+live_loop :imperial do
+  2.times do
+    3.times do
+      play:G4, rate:3
+      sleep 1
+    end
+    play:E4
+    sleep 0.75
+    small
+    
+    first
+    
+    3.times do
+      play:D5
+      sleep 1
+    end
+    play:Eb5
+    sleep 0.75
+    small
+    
+    second
+    
+    small
+    play:Gb4
+  end
+  stop
+end
+
+sleep 26
+
+x = 1
+live_loop :Darthv do
+  10.times do
+    sample darth ,  amp: x
+    sleep 1.5
+    x = x - 0.1
+    print x
+  end
+  x = 1
+  stop
+end
